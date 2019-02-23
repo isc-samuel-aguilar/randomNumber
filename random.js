0c72b3c8e414ca46c5f21cecf29c0f93d53d8456
sam_vaio <isc.samuel.aguilar@gmail.com>
2019-02-23 10:21:31 -0600

    Add comments and move down test function

--- random.js
+++ random.js
@@ -1,51 +1,46 @@
-function testRandomDigit(number){
-    loopNum = 0;
-    while ( randomDigit() !== number){
-        loopNum++;
-    } 
-    console.log('Loops:' + loopNum + "| Number" + number);
-}
-
+//Mandatory Function
 function flip(){
 	return Math.random() >= 0.5;
 }
 
+//function to validate if the input is an integer
 function isInt(value) {
     return !isNaN(value) && parseInt(Number(value)) == value && !isNaN(parseInt(value, 10));
 }
 
+//function to return a random single digit until the flip function return true
 randomDigit = function(){
     let i = -1;
     do {
-        if(i++ >= 10) i = 0;
+        if(i++ >= 10) i = 0; //Force to return numbers between 0-9
     } while ( flip() );
     return i;
 }
 
-
 var randomNumber = (maxNum) => {
-	//if (!isInt(maxNum) || parseInt(maxNum) === 0 || parseInt(maxNum)>=1000000 ){
-    if (!(maxNum => !isNaN(maxNum) && parseInt(Number(maxNum)) == maxNum && !isNaN(parseInt(maxNum, 10))) 
+    let topErrorNum = 1000000;    
+    let result = ""; //Sting to concatenate the random digits
+
+    //if (!isInt(maxNum) || parseInt(maxNum) === 0 || parseInt(maxNum)>=1000000 ){ //validate input throw isInt function 
+    if (!(maxNum => !isNaN(maxNum) && parseInt(Number(maxNum)) == maxNum && !isNaN(parseInt(maxNum, 10))) //lambda format function
         || parseInt(maxNum) === 0 
-        || parseInt(maxNum) >= 1000000 
+        || parseInt(maxNum) >= topErrorNum 
     ){
         throw "Wrong Input Value"
     }
 
-    let result = "";
-    let auxRand;
-
+    let lastRandom; //to store the last randomDigit(), required to prevent 2 equal digits together in the result
     //Execute until the length of the result is equal to length of input
     do {
-        if (result.length === 0 ){
+        if (result.length === 0 ){ //loop for the first digit and prevent that the returned value should be higher than provided and reduce loops on big numbers 
             do {
-                auxRand = randomDigit(); 
-            } while (auxRand >= maxNum.toString()[0]);
-            result += auxRand
+                lastRandom = randomDigit(); 
+            } while (lastRandom >= maxNum.toString()[0]);
+            result += lastRandom; 
         } else {
-            auxRand = randomDigit();
-            if (result.toString().slice(-1) !== auxRand.toString()){
-                result += auxRand;
+            lastRandom = randomDigit();
+            if (result.toString().slice(-1) !== lastRandom.toString()){
+                result += lastRandom;
             }      
         }
     } while( result.length < maxNum.toString().length);
@@ -55,15 +50,25 @@ var randomNumber = (maxNum) => {
     for (let i = 0; i < maxNum.toString().length; i++){
         if (i === 0 ){
             do {
-                auxRand = randomDigit(); 
-            } while (auxRand >= maxNum.toString()[0]);
-            result += auxRand
+                lastRandom = randomDigit(); 
+            } while (lastRandom >= maxNum.toString()[0]);
+            result += lastRandom
         } else {
             result += randomDigit();
         }
     }
     */
+
     console.log(result); //Used only to check Length Ex: input = 500 => result = 0031 => return 31
     return parseInt(result);
 }
 
+//Function used only for testing / development
+//function to test that the randomDigit() return numbers between 0-1 and check the number of loops
+function testRandomDigit(number){
+    loopNum = 0;
+    while ( randomDigit() !== number){
+        loopNum++;
+    } 
+    console.log('Loops:' + loopNum + "| Number" + number);
+}
